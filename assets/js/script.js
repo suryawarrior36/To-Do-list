17a196f78fdc978f2125843522c18f1686f1043a
SuryaWarrior26 <ssuryanarayanan301@gmail.com>
2024-03-03 23:22:10 +0530

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@ const inputEl = document.getElementById("task-content");
 const taskEl = document.getElementById("list-content");
 
 function printList() {
-    if (inputEl.value.trim() === '') {
+    if (inputEl.value === '') {
         alert("Please enter your task to submit!!");
     } else {
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
+        checkbox.style.borderRadius = "100px"
+        checkbox.style.backgroundColor = "red"
         const label = document.createElement('label');
         label.textContent = inputEl.value;
         const listItem = document.createElement('li');
